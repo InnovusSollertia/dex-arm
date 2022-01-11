df04c6bfae8256c45070710c5c771bca766d0823
forsvarer <jb723505@gmail.com>
2022-01-11 19:23:33 +0600

    2022/01/11_19/23

--- v2/src/listing-5.2.py
+++
@@ -1,7 +0,0 @@
-from pydexarm import Dexarm
-dexarm = Dexarm("COM6") # Подключение к Windows
-# device = Dexarm("/dev/tty.usbmodem3086337A34381") # Подключение к MacOS/Linux
-
-dexarm.go_home()
-dexarm.move_to(45, 205, -20) # A
-dexarm.go_home()
